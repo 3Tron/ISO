6451f570c319c4e5240abe056a704173c8f34455
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2018-02-04 09:59:33 +0100

    clean up test avg

--- test/avg.py
+++ test/avg.py
@@ -5,5 +5,5 @@ def get_avg(f):
     myimg = cv2.imread(f)
     avg_color_per_row = numpy.average(myimg, axis=0)
     avg_color = numpy.average(avg_color_per_row, axis=0)
-    print(f, avg_color, is_greenish(avg_color))
-
+    #print(f, avg_color)
+    return avg_color
